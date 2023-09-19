6180952c33d64089103e09a0b823334e6517fe8f
Glauber Marques <glauberfmarques@gmail.com>
2023-09-19 06:47:26 -0600

    Implement the game loop and insertlogic behind the game behavior

--- game.rb
+++ game.rb
@@ -7,8 +7,42 @@ class Game
   end
   
   def start
-  puts "Starting new game..."
-  # TODO: implement game loop
+    puts "Starting new game..."
+
+    loop do
+      puts "----- NEW TURN -----"
+
+      # Ask question
+      question = Question.new
+      question.generate
+      puts "#{@current_player.name}: #{question.prompt}"
+
+      # Get player's answer
+      answer = gets.chomp.to_i
+
+      # Check if correct  
+      if @current_player.correct_answer?(answer, question)
+        puts "#{@current_player.name}: YES! You are correct."
+      else
+        puts "#{@current_player.name}: Seriously? No!"
+        @current_player.lose_life
+      end
+
+      # Switch players
+      if @current_player == @player1
+        @current_player = @player2 
+      else
+        @current_player = @player1
+      end
+
+      # Check if game over
+      if @current_player.lives == 0
+        puts "#{@current_player.name} is out of lives. GAME OVER!"
+        puts "#{@other_player.name} wins with a score of #{@other_player.lives}/3"
+        break
+      end
+
+    end
   end
-  
-  end
+
+end
